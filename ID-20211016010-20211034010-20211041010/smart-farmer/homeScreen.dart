8755d3ebf5fb8cb16b79c081d0443628472db2fa
kamrul1027 <122284715+kamrul1027@users.noreply.github.com>
2023-03-22 12:45:33 -0700

    Add files via upload

---
+++ ID-20211016010-20211034010-20211041010/smart-farmer/homeScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class homepage extends StatefulWidget {
+  const homepage({Key? key}) : super(key: key);
+
+  @override
+  State<homepage> createState() => _homepageState();
+}
+
+class _homepageState extends State<homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text("home page"),)
+    );
+  }
+}
---
+++ ID-20211016010-20211034010-20211041010/smart-farmer/splash_screen.dart
@@ -0,0 +1,36 @@
+import 'package:farmer/consts/colors.dart';
+import 'package:farmer/consts/consts.dart';
+import 'package:farmer/widest_common/applogo_wid.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: green,
+      body: Center(
+        child: Column(
+          children: [
+            Align(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  icSplashBg,
+                  width: 300,
+                )),
+            20.heightBox,
+            applogoWidget(),
+          ],
+        ),
+      ),
+    );
+  }
+}
