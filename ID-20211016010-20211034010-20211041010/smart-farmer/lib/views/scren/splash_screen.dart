d882f5eed1ce95f72f1f574c4ff6360b04c338e6
kamrul1027 <122284715+kamrul1027@users.noreply.github.com>
2023-03-25 22:20:54 -0700

    Update splash_screen.dart

--- ID-20211016010-20211034010-20211041010/smart-farmer/lib/views/scren/splash_screen.dart
+++ ID-20211016010-20211034010-20211041010/smart-farmer/lib/views/scren/splash_screen.dart
@@ -20,7 +20,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   changeScreen() {
-    Future.delayed(Duration(seconds: 3), () {
+    Future.delayed(Duration(seconds: 1.5), () {
       Get.to(() => const LoginScreen());
     });
   }
