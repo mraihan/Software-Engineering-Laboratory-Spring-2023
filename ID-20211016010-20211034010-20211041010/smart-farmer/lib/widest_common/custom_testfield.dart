01ac6eb3cbd18d0000775ab013d1b2d9f8c7a0b7
kamrul1027 <122284715+kamrul1027@users.noreply.github.com>
2023-03-25 22:25:13 -0700

    Update custom_testfield.dart

--- ID-20211016010-20211034010-20211041010/smart-farmer/lib/widest_common/custom_testfield.dart
+++ ID-20211016010-20211034010-20211041010/smart-farmer/lib/widest_common/custom_testfield.dart
@@ -1,12 +1,14 @@
 import 'package:farmer/consts/consts.dart';
 
-Widget customtextfield({String? title, String? hint, controller}) {
+Widget customtextfield({String? title, String? hint, controller, isPass}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
       title!.text.color(redColor).fontFamily(semibold).size(16).make(),
       5.heightBox,
       TextFormField(
+        obscureText: isPass,
+        controller: controller,
         decoration: InputDecoration(
           hintStyle: TextStyle(
             fontFamily: semibold,
