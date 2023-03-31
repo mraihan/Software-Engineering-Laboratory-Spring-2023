4baf931c277e8950c627beeec834a2f6b81419c9
DhuruboDev <122600914+DhuruboDev@users.noreply.github.com>
2023-04-01 00:18:11 +0600

    Update app.py

--- Id-20211025010-20211027010/Driver_Drowsiness_System/app.py
+++ Id-20211025010-20211027010/Driver_Drowsiness_System/app.py
@@ -16,33 +16,6 @@ def process():
     detector = dlib.get_frontal_face_detector()
     predictor = dlib.shape_predictor(pwd + '/shape_predictor_68_face_landmarks.dat')
     
-    cap = cv2.VideoCapture(0)
-
-    while True:
-        _, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-        faces = detector(gray)
-        for face in faces:
-            x1 = face.left()
-            y1 = face.top()
-            x2 = face.right()
-            y2 = face.bottom()
-            #cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
-
-            landmarks = predictor(gray, face)
-
-            for n in range(0, 68):
-                x = landmarks.part(n).x
-                y = landmarks.part(n).y
-                cv2.circle(frame, (x, y), 2, (255, 0, 0), -2)
-
-        cv2.imshow("Frame", frame)
-
-        key = cv2.waitKey(1)
-        if key == 27:
-            break
-    
     cap.release()
     cv2.destroyAllWindows()
 
