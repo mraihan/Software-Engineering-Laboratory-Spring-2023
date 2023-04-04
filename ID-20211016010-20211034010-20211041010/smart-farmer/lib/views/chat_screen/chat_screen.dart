82ca2bc54ff5b6bfe38a93914a0539664918f9da
kamrul1027 <122284715+kamrul1027@users.noreply.github.com>
2023-04-05 01:18:46 +0600

    Add files via upload

---
+++ ID-20211016010-20211034010-20211041010/smart-farmer/lib/views/chat_screen/chat_screen.dart
@@ -0,0 +1,106 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:farmer/consts/consts.dart';
+import 'package:farmer/controller/chats_controller.dart';
+import 'package:farmer/services/firestore_services.dart';
+import 'package:farmer/views/chat_screen/component/sender_bubble.dart';
+import 'package:farmer/widest_common/loading_indicator.dart';
+import 'package:get/get.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var controller = Get.put(chatController());
+    return Scaffold(
+      backgroundColor: whiteColor,
+      appBar: AppBar(
+        title: "${controller.friendName}"
+            .text
+            .fontFamily(semibold)
+            .color(darkFontGrey)
+            .make(),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Obx(
+              () => controller.isloading.value
+                  ? Center(
+                      child: loadingindicator(),
+                    )
+                  : Expanded(
+                      child: StreamBuilder(
+                        stream: FireStoreServices.getchatMessage(
+                            controller.chatDocId.toString()),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (!snapshot.hasData) {
+                            return Center(
+                              child: loadingindicator(),
+                            );
+                          } else if (snapshot.data!.docs.isEmpty) {
+                            return Center(
+                              child: "Send a message....."
+                                  .text
+                                  .color(darkFontGrey)
+                                  .make(),
+                            );
+                          } else {
+                            return ListView(
+                              children: snapshot.data!.docs
+                                  .mapIndexed((currentValue, index) {
+                                var data = snapshot.data!.docs[index];
+                                return Align(
+                                    alignment: data['uid'] == currentUser!.uid
+                                        ? Alignment.centerRight
+                                        : Alignment.bottomLeft,
+                                    child: senderBubble(data));
+                              }).toList(),
+                            );
+                          }
+                        },
+                      ),
+                    ),
+            ),
+            10.heightBox,
+            Row(
+              children: [
+                Expanded(
+                    child: TextFormField(
+                  controller: controller.msgController,
+                  decoration: const InputDecoration(
+                    border: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: textfieldGrey,
+                    )),
+                    focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(
+                      color: textfieldGrey,
+                    )),
+                    hintText: "Type a message..........",
+                  ),
+                )),
+                IconButton(
+                    onPressed: () {
+                      controller.SendMsg(controller.msgController.text);
+                      controller.msgController.clear();
+                    },
+                    icon: const Icon(
+                      Icons.send,
+                      color: redColor,
+                    ))
+              ],
+            )
+                .box
+                .height(80)
+                .padding(const EdgeInsets.all(12))
+                .margin(const EdgeInsets.only(bottom: 8))
+                .make(),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ ID-20211016010-20211034010-20211041010/smart-farmer/lib/views/chat_screen/component/sender_bubble.dart
@@ -0,0 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:farmer/consts/consts.dart';
+import 'package:intl/intl.dart' as intl;
+
+Widget senderBubble(DocumentSnapshot data) {
+  var t =
+      data['created_on'] == null ? DateTime.now() : data['created_on'].toDate();
+  var time = intl.DateFormat("h.mma").format(t);
+  return Directionality(
+    textDirection:
+        data['uid'] == currentUser!.uid ? TextDirection.rtl : TextDirection.ltr,
+    child: Container(
+      padding: const EdgeInsets.all(8),
+      margin: const EdgeInsets.only(bottom: 8),
+      decoration: BoxDecoration(
+          color: data['uid'] == currentUser!.uid
+              ? Color.fromARGB(88, 180, 1, 1)
+              : darkFontGrey,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+            bottomLeft: Radius.circular(20),
+            bottomRight: Radius.circular(20),
+          )),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          "${data['msg']}".text.size(16).white.make(),
+          10.heightBox,
+          time.text.color(whiteColor.withOpacity(0.5)).make(),
+        ],
+      ),
+    ),
+  );
+}
