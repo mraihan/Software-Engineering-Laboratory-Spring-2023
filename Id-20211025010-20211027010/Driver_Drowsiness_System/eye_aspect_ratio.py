c52dadf579dd7f48d989a0c6950083f2b6e0fc11
DhuruboDev <122600914+DhuruboDev@users.noreply.github.com>
2023-04-05 20:56:56 +0600

    Add files via upload

---
+++ Id-20211025010-20211027010/Driver_Drowsiness_System/eye_aspect_ratio.py
@@ -0,0 +1,6 @@
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+    ear = (A + B) / (2.0 * C)
+    return ear
