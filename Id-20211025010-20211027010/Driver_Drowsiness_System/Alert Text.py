6f5a8f976e91f05da6a6d3a4f5769fd0a888511f
Masum Hossain <116505615+masum0410@users.noreply.github.com>
2023-04-05 20:52:43 +0600

    Add files via upload

---
+++ Id-20211025010-20211027010/Driver_Drowsiness_System/Alert Text.py
@@ -0,0 +1,15 @@
+ leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            
+            if ear < thresh:
+                flag += 1
+                print (flag)
+                if flag >= frame_check:
+                    cv2.putText(frame, "***************ALERT!***************", (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    cv2.putText(frame, "***************ALERT!***************", (10,325),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                    mixer.music.play()
+            else:
+                flag = 0
