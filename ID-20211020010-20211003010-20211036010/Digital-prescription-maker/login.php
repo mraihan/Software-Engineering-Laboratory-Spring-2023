d4f5d5df995915e72a4aaee92ca2c8629b12b9ca
Rahul Biswas <122615641+RahulBiswas4@users.noreply.github.com>
2023-04-07 17:23:08 +0600

    Add files via upload

---
+++ ID-20211020010-20211003010-20211036010/Digital-prescription-maker/login.php
@@ -0,0 +1,97 @@
+
+<?php  
+session_start();
+
+if(isset($_SESSION["uid"]) && $_SESSION["uid"] != ''){
+
+   header("Location:index.php");
+
+}
+ 
+include_once 'inc/db.php';
+
+if(isset($_POST['login'])){
+
+ 
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+
+  $sel = "SELECT * FROM `user` where email='$email' and password='$password'";
+  $res = mysqli_query($con,$sel);
+  $row = mysqli_fetch_array($res);
+
+   if($res){
+
+
+      if(mysqli_num_rows($res)>0){
+ 
+        $_SESSION["uid"] =$row['id'];
+        $_SESSION["type"] =$row['type'];
+        $_SESSION["uuid"] =$row['uuid'];
+        header("Location:index.php");
+ 
+      }
+   }
+
+
+
+
+
+}
+ 
+
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Digital prescription</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  body {
+    
+    background: #aaa;
+  }
+  </style>
+</head>
+<body  >
+
+<div class="container mt-5" >
+
+          <div style="width: 400px;padding:16px;background:#fff;margin:auto">
+
+                 <h2>Login</h2>
+
+                <form action="" method="post">
+                <div class="mb-3 mt-3">
+                  <label for="email" class="form-label">Email:</label>
+                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <div class="mb-3">
+                  <label for="pwd" class="form-label">Password:</label>
+                  <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
+                </div>
+                <div class="form-check mb-3">
+                  <label class="form-check-label">
+                    Don't have account ? <a href="/prescription/signup.php">Signup</a>
+                  </label>
+                </div>
+                <button type="submit" name="login" class="btn btn-primary">Login Now!</button>
+              </form>
+
+          </div>
+ 
+</div>
+ 
+ 
+
+</body>
+</html>
+
+
---
+++ ID-20211020010-20211003010-20211036010/Digital-prescription-maker/signup.php
@@ -0,0 +1,115 @@
+<?php  
+session_start();
+
+if($_SESSION["uid"] != ''){
+
+    header("Location:index.php");
+
+}
+ 
+include_once 'inc/db.php';
+
+if(isset($_POST['signup'])){
+
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  $type = $_POST['type'];
+  $uuid = rand(10000,99999);
+
+  $insert  = "INSERT INTO `user`(`name`, `email`, `password`,`type`,`uuid`)
+   VALUES ('$name','$email','$password','$type','$uuid')";
+
+   $res = mysqli_query($con,$insert);
+
+   if($res){
+
+        $sel = "SELECT * FROM `user` where email='$email' and password='$password'";
+        $res = mysqli_query($con,$sel);
+        $row = mysqli_fetch_array($res);
+
+        $_SESSION["uid"] =$row['id'];
+        $_SESSION["type"] =$row['type'];
+        $_SESSION["uuid"] =$row['uuid'];
+     
+        header("Location:index.php");
+
+
+
+   }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Digital prescription</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <style>
+  body {
+    
+    background: #aaa;
+  }
+  </style>
+</head>
+<body>
+
+<div class="container mt-5" >
+
+          <div style="width: 400px;padding:16px;background:#fff;margin:auto">
+
+                 <h2>Create Account </h2>
+
+                <form action="" method="post">
+                <div class="mb-3 mt-3">
+                  <label for="email" class="form-label">Name:</label>
+                  <input type="text" class="form-control" id="email" placeholder="Enter Name" name="name">
+                </div>
+               
+                <div class="mb-3 mt-3">
+                  <label for="email" class="form-label">Email:</label>
+                  <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                </div>
+                <div class="mb-3">
+                  <label for="pwd" class="form-label">Password:</label>
+                  <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="password">
+                </div>
+                <div class="mb-3">
+                  <label for="pwd" class="form-label">Account type:</label>
+                  <select class="form-control" name="type">
+                    <option value="Doctor">Doctor</option>
+                    <option value="Compounder">Compounder</option>
+                  </select>
+                </div>
+                <button type="submit" name="signup" class="btn btn-primary">Signup Now!</button>
+              </form>
+
+          </div>
+ 
+</div>
+ 
+ 
+
+</body>
+</html>
+
+
