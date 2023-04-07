55d0acf6f9993471f675a3c9c7eb32b5396008e5
swadhin321 <124994091+swadhin321@users.noreply.github.com>
2023-04-07 06:51:38 -0600

    Add files via upload

--- ID 20211022010-20211035010-20211015010/Modern Education/logout.php
+++ ID 20211022010-20211035010-20211015010/Modern Education/logout.php
@@ -1,5 +1,5 @@
 <?php 
-require_once '../include/initialize.php';
+require_once 'include/initialize.php';
 // Four steps to closing a session
 // (i.e. logging out)
 
@@ -7,19 +7,14 @@ require_once '../include/initialize.php';
 @session_start();
 
 // 2. Unset all the session variables
-unset( $_SESSION['USERID'] );
-unset( $_SESSION['NAME'] );
-unset( $_SESSION['UEMAIL'] );
-unset( $_SESSION['PASS'] );
-unset( $_SESSION['TYPE'] );
-// unset($_SESSION['fixnmix_cart']);
-// unset($_SESSION['fixnmix_carttwo']);
-// unset($_SESSION['FIRSTNAME']);
-// unset($_SESSION['LASTNAME']);
-// unset($_SESSION['ADDRESS']);
-// unset($_SESSION['CONTACTNUMBER']);
+unset( $_SESSION['StudentID'] );
+unset( $_SESSION['Fname'] );
+unset( $_SESSION['Lname'] ); 
+unset( $_SESSION['STUDUSERNAME'] );  
+unset( $_SESSION['STUDPASS'] );  
+ 
  	
 // 4. Destroy the session
 // session_destroy();
-redirect(web_root."admin/login.php?logout=1");
+redirect("index.php?logout=1");
 ?>
